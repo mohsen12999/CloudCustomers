5742ea5e7d02c2101831abb58b7aa67bfa81358a
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-06-20 20:44:41 +0430

    test output of service

--- CloudCustomers.UnitTests/Systems/Services/TestUsersServices.cs
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersServices.cs
@@ -1,10 +1,12 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using CloudCustomers.API.Models;
 using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Helpers;
+using FluentAssertions;
 using Moq;
 using Moq.Protected;
 using Xunit;
@@ -35,5 +37,21 @@ public class TestUsersServices
                 ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
                 ItExpr.IsAny<CancellationToken>()
                 );
+    }
+
+    [Fact]
+    public async Task GetAllUser_WhenCalled_ReturnListOfUsers()
+    {
+        // Arrange
+        var expectedResponse = UsersFixture.GetTestUsers();
+        var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UsersService(httpClient);
+        
+        // Act
+        var result = await sut.GetAllUsers();
+
+        // Assert
+        result.Should().BeOfType<List<User>>();
     }
 }
